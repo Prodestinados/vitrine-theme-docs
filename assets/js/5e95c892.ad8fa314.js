944b4cded6fa6e1458d679f4b0e0ab2fd2f400a3
Prodestinados <Prodestinados@users.noreply.github.com>
2026-04-06 17:41:51 +0000

    deploy: d892c6326615da229411cd981293d70369e340fb

---
+++ assets/js/5e95c892.ad8fa314.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkvitrine_theme_docs=globalThis.webpackChunkvitrine_theme_docs||[]).push([[9647],{7121(e,s,r){r.r(s),r.d(s,{default:()=>u});r(6540);var t=r(4164),a=r(7559),c=r(5500),i=r(2831),h=r(1656),n=r(4848);function u(e){return(0,n.jsx)(c.e3,{className:(0,t.A)(a.G.wrapper.docsPages),children:(0,n.jsx)(h.A,{children:(0,i.v)(e.route.routes)})})}}}]);
---
+++ assets/js/aba21aa0.94a3e4f5.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkvitrine_theme_docs=globalThis.webpackChunkvitrine_theme_docs||[]).push([[5742],{7093(e){e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
